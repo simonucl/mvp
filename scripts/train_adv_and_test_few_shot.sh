49b53f66294d724c02d68a54c101395810b7725e
simon <simonucl>
2023-08-21 00:05:40 +0100

    fixed bugs

--- scripts/train_adv_and_test_few_shot.sh
+++ scripts/train_adv_and_test_few_shot.sh
@@ -41,7 +41,7 @@ do
 
             mkdir -p ${MODELPATH}
 
-            nohup python main.py  --mode $MODE \
+            nohup python3 main.py  --mode $MODE \
                     --dataset $DATASET \
                     --model_type $MODEL_TYPE \
                     --model_id $MODEL_ID \
@@ -61,26 +61,26 @@ do
                     --val_size $VAL_SIZE \
                     --dataset_path $DATASET_PATH > ${MODELPATH}/logs_trainer.txt
 
-            # nohup nice -n10 python main.py --mode attack \
-            #                             --path ${MODELPATH}/final_model/ \
-            #                             --attack_name textfooler \
-            #                             --num_examples 1000 --dataset ${DATASET} \
-            #                             --query_budget -1 --batch_size ${BATCH_SIZE} --model_type ${MODEL_TYPE} --model ${MODEL} \
-            #                             --pool_label_words ${POOL_LABELS_TEST} --pool_templates ${POOL_TEMPLATES_TEST} \
-            #                             --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE} \
-            #                             --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_textfooler.txt
+            nohup nice -n10 python3 main.py --mode attack \
+                                        --path ${MODELPATH}/final_model/ \
+                                        --attack_name textfooler \
+                                        --num_examples 1000 --dataset ${DATASET} \
+                                        --query_budget -1 --batch_size ${BATCH_SIZE} --model_type ${MODEL_TYPE} --model ${MODEL} \
+                                        --pool_label_words ${POOL_LABELS_TEST} --pool_templates ${POOL_TEMPLATES_TEST} \
+                                        --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE} \
+                                        --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_textfooler.txt
 
 
-            # nohup nice -n10 python main.py --mode attack \
-            #                         --path ${MODELPATH}/final_model/ \
-            #                         --attack_name textbugger \
-            #                         --num_examples 1000 --dataset ${DATASET} \
-            #                         --query_budget -1 --batch_size ${BATCH_SIZE} --model_type ${MODEL_TYPE} --model ${MODEL} \
-            #                         --pool_label_words ${POOL_LABELS_TEST} --pool_templates ${POOL_TEMPLATES_TEST} \
-            #                         --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE}  \
-            #                         --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_textbugger.txt
+            nohup nice -n10 python3 main.py --mode attack \
+                                    --path ${MODELPATH}/final_model/ \
+                                    --attack_name textbugger \
+                                    --num_examples 1000 --dataset ${DATASET} \
+                                    --query_budget -1 --batch_size ${BATCH_SIZE} --model_type ${MODEL_TYPE} --model ${MODEL} \
+                                    --pool_label_words ${POOL_LABELS_TEST} --pool_templates ${POOL_TEMPLATES_TEST} \
+                                    --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE}  \
+                                    --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_textbugger.txt
 
-            nohup nice -n10 python main.py --mode attack \
+            nohup nice -n10 python3 main.py --mode attack \
                                     --path ${MODELPATH}/final_model/ \
                                     --attack_name bae \
                                     --num_examples 1000 --dataset ${DATASET} \
@@ -96,7 +96,7 @@ do
             MODELPATH=./checkpoints/${DATASET}/${MODEL}/model_${MODEL_ID}/
 
             mkdir -p ${MODELPATH}
-            nohup python main.py  --mode $MODE \
+            nohup python3 main.py  --mode $MODE \
                             --dataset $DATASET \
                             --model_type $MODEL_TYPE \
                             --model_id $MODEL_ID \
@@ -121,7 +121,7 @@ do
 
             
 
-            nohup nice -n10 python main.py --mode attack \
+            nohup nice -n10 python3 main.py --mode attack \
                                         --path ${MODELPATH}/final_model/ \
                                         --attack_name textfooler \
                                         --num_examples -1 --dataset ${DATASET} \
@@ -131,7 +131,7 @@ do
                                         --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_textfooler.txt
 
 
-            nohup nice -n10 python main.py --mode attack \
+            nohup nice -n10 python3 main.py --mode attack \
                                     --path ${MODELPATH}/final_model/ \
                                     --attack_name textbugger \
                                     --num_examples -1 --dataset ${DATASET} \
@@ -140,6 +140,14 @@ do
                                     --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE}  \
                                     --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_textbugger.txt
 
+            nohup nice -n10 python3 main.py --mode attack \
+                                    --path ${MODELPATH}/final_model/ \
+                                    --attack_name bae \
+                                    --num_examples -1 --dataset ${DATASET} \
+                                    --query_budget -1 --batch_size ${BATCH_SIZE} --model_type ${MODEL_TYPE} --model ${MODEL} \
+                                    --pool_label_words ${POOL_LABELS_TEST} --pool_templates ${POOL_TEMPLATES_TEST} \
+                                    --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE}  \
+                                    --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} --seed $SEED > ${MODELPATH}/logs_bae.txt
         done
     done
 done
