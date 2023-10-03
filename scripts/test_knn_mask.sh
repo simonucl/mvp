4fe3678526689cb8d03ea530abc3d0c246b14f51
Wenyu Huang <co-huan1@login-e-4.data.cluster>
2023-10-03 15:10:37 +0100

    afjieo

--- scripts/test_knn_mask.sh
+++ scripts/test_knn_mask.sh
@@ -59,7 +59,7 @@ do
                                     --verbalizer_file ${VERBALIZER_FILE} --template_file ${TEMPLATE_FILE} \
                                     --num_template ${NUM_TEMPLATE} --train_size ${TRAIN_SIZE} --val_size ${VAL_SIZE} \
                                     --seed $SEED --knn_model ${MODEL} --epsilon $EPSILON --norm $NORM \
-                                    --mask_augment $MASK > ${MODELPATH}/logs_textfooler_beta_${BETA}.txt
+                                    --mask_augment > ${MODELPATH}/logs_textfooler_beta_${BETA}.txt
 
         # nohup nice -n10 python3 main.py --mode attack \
         #                             --path ${MODELPATH}/final_model/ \
