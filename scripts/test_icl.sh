c86c04c45f8c41ce4336c8e1991dfa82af4268e1
Simon <simon011130@gmail.com>
2023-10-19 14:11:20 +0000

    add stuff

--- scripts/test_icl.sh
+++ scripts/test_icl.sh
@@ -14,9 +14,9 @@ ADV=${8}
 
 # export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/software/spack/spack-rhel8-20210927/opt/spack/linux-centos8-zen2/gcc-9.4.0/cuda-11.4.0-3hnxhjt2jt4ruy75w2q4mnvkw7dty72l
 
-for SEED in 13;
+for SEED in 1 42;
 do
-    for SHOT in 4 8;
+    for SHOT in 4 8 16;
     do 
         echo $SEED+${SHOT}+${MODEL}+"mvp"
         # if [[ $ADV -eq 1 ]]; then
@@ -37,7 +37,11 @@ do
 
         # MODEL_TYPE=knn_icl
         KNN=4
-        BATCH_SIZE=4
+        BATCH_SIZE=8
+        if [[ $SHOT -eq 16 ]]; then
+            BATCH_SIZE=4
+        fi
+        
         nohup python3 main.py --mode attack \
                                     --attack_name ${ATTACK} --num_examples 1000 --dataset ${DATASET} \
                                     --query_budget -1 --batch_size ${BATCH_SIZE} --model_type ${MODEL_TYPE} --model ${MODEL} \
